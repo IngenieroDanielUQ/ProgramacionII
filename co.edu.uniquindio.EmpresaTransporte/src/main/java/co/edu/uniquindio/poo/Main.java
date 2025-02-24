86fc118c2148877a1e3c9ea482a23db2917dadb6
Héctor Daniel Gonzalez Sánchez <hectord.gonzalezs@uqvirtual.edu.co>
2025-02-23 19:18:08 -0500

    Eliminacion calculadora

--- co.edu.uniquindio.EmpresaTransporte/src/main/java/co/edu/uniquindio/poo/Main.java
+++ co.edu.uniquindio.EmpresaTransporte/src/main/java/co/edu/uniquindio/poo/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 /**
  * Ejercicios de Repaso
  * autor: Hector Daniel Gonzalez Sanchez
- *  IntelliJ
- *
+ * IntelliJ
  */
 public class Main {
     public static void main(String[] args) {
